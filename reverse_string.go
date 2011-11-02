83ba7312a40ecce6a9c514f06899abb71cfb71a0
Bharat Shetty Barkur <bharat.shetty@gmail.com>
2011-11-01 23:28:06 -0700

    reverse of a simple string input

---
+++ reverse_string.go
@@ -0,0 +1,30 @@
+/*
+ * Reversing a simple string
+ * Lessons:
+ * 	- Strings in go are immutable
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+func reverse_str(orig_str string) string {
+	ls := []int(orig_str) // make array
+	str_len := len(ls)
+	
+	for i:=0 ; i< str_len /2 ; i++ {
+		// Swap :O
+		ls[i],ls[str_len-i-1] = ls[str_len-i-1],ls[i]
+	}
+    	return string(ls) // Inbuild function to convert to string
+}
+
+func main() {
+	fmt.Println("Enter the string you want to be reversed without any spaces")
+	var orig_str string 
+	fmt.Scan(&orig_str)
+	reversed_str := reverse_str(orig_str)
+	fmt.Println(reversed_str)	
+}
